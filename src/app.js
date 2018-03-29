eda53d6d95b75f7a440daf7a625d090f372f53c2
Paulo Santos <up201403745@fe.up.pt>
2018-03-29 16:19:29 +0100

    adding sequelize

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+const Sequelize = require('sequelize');
 
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME_DEV, process.env.DB_USER_DEV, process.env.DB_PASS_DEV, {
+  host: process.env.DB_HOST_DEV,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
-
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
