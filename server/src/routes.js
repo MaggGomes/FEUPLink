2285761795f91d3a555d2fceeeb181074187389d
Jose Martins <lluismmartins7@gmail.com>
2018-03-29 19:19:00 +0100

    added register rout

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,9 +5,19 @@ module.exports = (app) => {
     });
   });
 
+  // ----Authentication
+
   app.get('/team', function(req, res) {
     res.send({
       message: 'hello team-c',
     });
   });
+
+  app.get('/register', function(req, res) {
+    res.send({
+      message: `hello ${req.body.email}`,
+    });
+  });
+
+  //
 };
