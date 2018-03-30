bc02bf4bc130935829ec200033f5c6a997f1589c
Nuno Ramos <nunor07.unfc@gmail.com>
2018-03-30 20:41:56 +0100

    Adding vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// Importing vuetify stuff
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
