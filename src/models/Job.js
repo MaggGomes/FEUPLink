3f70d876f2bb31f374167d7eebb4fac61d7dd7af
Jose Martins <lluismmartins7@gmail.com>
2018-04-02 19:28:45 +0100

    finalizing databas model with the ORM(sequelize)

---
+++ src/models/Job.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const Job = sequelize.define('Job', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    isCurrent: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+  );
+
+
+  return Job;
+};
