8879c02e8e8f57a643c325ad67553444b12893fc
Paulo Santos <up201403745@fe.up.pt>
2018-04-06 18:14:33 +0100

    Fixed some errors in signup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,34 +1,36 @@
 <template>
-  <div id="app">
-  
-    <v-navigation-drawer app temporary v-model="sideNav" absolute>
-      <v-list>
-        <v-list-tile v-for="item in menuItens" :key="item.title" route :to="item.link">
-          <v-list-tile-content>
-            {{ item.title }}
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-  
-    <v-toolbar app dark class="red darken-4">
-      <v-toolbar-side-icon class="hidden-sm-and-up" @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
-      <v-toolbar-title class="font-color">
-        <router-link to="/" tag="span" style="cursor: pointer">FEUPLink</router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItens" :key="item.title" router :to="item.link">{{ item.title }}</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-  
-    <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-    <v-footer app></v-footer>
-  </div>
+  <v-app>
+    <div id="app">
+    
+      <v-navigation-drawer app temporary v-model="sideNav" absolute>
+        <v-list>
+          <v-list-tile v-for="item in menuItens" :key="item.title" route :to="item.link">
+            <v-list-tile-content>
+              {{ item.title }}
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+    
+      <v-toolbar app dark class="red darken-4">
+        <v-toolbar-side-icon class="hidden-sm-and-up" @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+        <v-toolbar-title class="font-color">
+          <router-link to="/" tag="span" style="cursor: pointer">FEUPLink</router-link>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-xs-only">
+          <v-btn flat v-for="item in menuItens" :key="item.title" router :to="item.link">{{ item.title }}</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </div>
+  </v-app>
 </template>
 
 <script>
--- client/src/components/pages/SignUpContinue.vue
+++ client/src/components/pages/SignUpContinue.vue
@@ -1,62 +1,80 @@
 <template>
-  <v-stepper v-model="e1">
-    <v-stepper-header>
-      <v-stepper-step step="1" :complete="e1 > 1">Personal</v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step step="2" :complete="e1 > 2">Profile type</v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step step="3" :complete="e1 > 3">In FEUP</v-stepper-step>
-      <v-divider></v-divider>
-      <v-stepper-step step="4">Experience</v-stepper-step>
-    </v-stepper-header>
-    <v-stepper-items>
-      <v-stepper-content step="1">
-        <v-card color="grey lighten-3" class="mb-4" height="200px">
-          <form autocomplete="off">
-                <v-menu
-                  ref="menu"
-                  :close-on-content-click="false"
-                  v-model="menu"
-                  transition="scale-transition"
-                  :nudge-right="40"
-                  min-width="290px"
-                >
-                  <v-text-field
-                    slot="activator"
-                    label="Birthday date"
-                    v-model="date"
-                    prepend-icon="event"
-                    readonly
-                  ></v-text-field>
-                  <v-date-picker
-                    ref="picker"
-                    v-model="date"
-                    @change="save"
-                    min="1950-01-01"
-                    :max="new Date().toISOString().substr(0, 10)"
-                  ></v-date-picker>
-                </v-menu>
-              </form>
-        </v-card>
-        <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>
-      </v-stepper-content>
-      <v-stepper-content step="2">
-        <v-card color="grey lighten-3" class="mb-5" height="200px"></v-card>
-        <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn>
-        <v-btn @click.native="e1 = 1" flat>Back</v-btn>
-      </v-stepper-content>
-      <v-stepper-content step="3">
-        <v-card color="grey lighten-3" class="mb-5" height="200px"></v-card>
-        <v-btn color="primary" @click.native="e1 = 4">Continue</v-btn>
-        <v-btn @click.native="e1 = 2" flat>Back</v-btn>
-      </v-stepper-content>
-      <v-stepper-content step="4">
-        <v-card color="grey lighten-3" class="mb-5" height="200px"></v-card>
-        <v-btn color="primary" @click.native="e1 = 1">Finish</v-btn>
-        <v-btn @click.native="e1 = 3" flat>Back</v-btn>
-      </v-stepper-content>
-    </v-stepper-items>
-  </v-stepper>
+  <v-app>
+    <v-stepper v-model="e1">
+      <v-stepper-header>
+        <v-stepper-step step="1" :complete="e1 > 1">Personal</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step step="2" :complete="e1 > 2">Profile type</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step step="3" :complete="e1 > 3">In FEUP</v-stepper-step>
+        <v-divider></v-divider>
+        <v-stepper-step step="4">Experience</v-stepper-step>
+      </v-stepper-header>
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-card color="grey lighten-3" class="mb-4" height="400px">
+            <form autocomplete="off">
+                  <v-container fluid>
+                    <v-menu
+                      ref="menu"
+                      lazy
+                      :close-on-content-click="false"
+                      v-model="menu"
+                      transition="scale-transition"
+                      offset-y
+                      full-width
+                      :nudge-right="40"
+                      min-width="290px"
+                    >
+                      <v-text-field
+                        slot="activator"
+                        label="Birthday date"
+                        v-model="date"
+                        prepend-icon="event"
+                        readonly
+                      ></v-text-field>
+                      <v-date-picker
+                        ref="picker"
+                        v-model="date"
+                        @change="save"
+                        min="1950-01-01"
+                        :max="new Date().toISOString().substr(0, 10)"
+                      ></v-date-picker>
+                    </v-menu>
+                  </v-container>
+                  <v-container fluid>
+                    <v-select
+                      :items="genders"
+                      v-model="e2"
+                      label="Gender"
+                      single-line
+                      auto
+                      prepend-icon="person"
+                      hide-details
+                    ></v-select>
+                  </v-container>
+                </form>
+          </v-card>
+          <v-btn color="primary" @click.native="e1 = 2">Continue</v-btn>
+        </v-stepper-content>
+        <v-stepper-content step="2">
+          <v-card color="grey lighten-3" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click.native="e1 = 3">Continue</v-btn>
+          <v-btn @click.native="e1 = 1" flat>Back</v-btn>
+        </v-stepper-content>
+        <v-stepper-content step="3">
+          <v-card color="grey lighten-3" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click.native="e1 = 4">Continue</v-btn>
+          <v-btn @click.native="e1 = 2" flat>Back</v-btn>
+        </v-stepper-content>
+        <v-stepper-content step="4">
+          <v-card color="grey lighten-3" class="mb-5" height="200px"></v-card>
+          <v-btn color="primary" @click.native="e1 = 1">Finish</v-btn>
+          <v-btn @click.native="e1 = 3" flat>Back</v-btn>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-app>
 </template>
 
 <script>
@@ -66,8 +84,10 @@ import { bus } from '@/main'
     data () {
       return {
         e1: 0,
+        e2: null,
         date: null,
-        menu: false
+        menu: false,
+        genders: ['Male', 'Female', 'Not Specified' ]
       }
     },
     created () {
@@ -89,4 +109,7 @@ import { bus } from '@/main'
 </script>
 
 <style scopped>
+.v-select .dropdown-menu {
+  display:block;
+}
 </style>
