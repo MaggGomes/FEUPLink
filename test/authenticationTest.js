600f951b58ded6274b796d797963638948105f43
unknown <magg.slb@gmail.com>
2018-04-06 04:26:25 +0100

    mocha and chai installed and example test implemented

---
+++ test/authenticationTest.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+const app = require('../src/app.js');
+
+describe('Authentication', function(){
+    it('should be a first test', function () {
+        assert.equal(1, 1);
+    });
+});
