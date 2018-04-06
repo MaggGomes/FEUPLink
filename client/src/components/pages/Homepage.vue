40aa409a68eb55283838e4b1d49f2b01da3cc3e9
Unknown <marianadguimaraes@hotmail.com>
2018-04-06 16:44:53 +0100

    carousel of homepage

--- client/src/components/pages/Homepage.vue
+++ client/src/components/pages/Homepage.vue
@@ -2,15 +2,56 @@
   <div class="text-lg-center text-md-center text-sm-center">
     <h1>FEUPLink</h1>
     <h3>Bem-vindo antigo aluno da FEUP!</h3>
+    <div id="carousel_homepage">
+      <v-carousel light="true" hide-delimiters="true" prev-icon="fas fa-caret-left " next-icon="fas fa-caret-right"  style="height: 100%">
+        <v-carousel-item
+                v-for="(item,i) in items"
+                :src="item.src"
+                :key="i"
+                transition="fade"
+                reverse-transition="fade"
+        >
+        </v-carousel-item>
+      </v-carousel>
+      <p>The FEUPLink community encompasses all those who are part of this huge family that is FEUP, from current and former students to teachers and collaborators. <a href="...">Join us!</a></p>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Homepage'
+    name: 'Homepage',
+    data () {
+        return {
+            items: [
+                {
+                    src: 'https://paginas.fe.up.pt/~feuplink/wp-content/uploads/2016/09/01.jpg'
+                },
+                {
+                    src: 'https://paginas.fe.up.pt/~feuplink/wp-content/uploads/2017/10/1975.jpg'
+                },
+                {
+                    src: 'https://paginas.fe.up.pt/~feuplink/wp-content/uploads/2016/07/01_Af.jpg'
+                }
+
+            ]
+        }
+    }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ #carousel_homepage{
+   height: 500px;
+  }
+ p{
+   margin-top:10px;
+   margin-bottom: 10px;
+ }
+ p>a{
+   text-decoration: none;
+    color:red;
+ }
 </style>
+
