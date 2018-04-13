3d0466e32a9c77c3f7e39e3b5b157f45431dac31
Paulo Santos <up201403745@fe.up.pt>
2018-04-13 11:54:15 +0100

    Registration process improved

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,4 +1,5 @@
 const {Person} = require('../models');
+const {Department} = require('../models');
 const jwt = require('jsonwebtoken');
 // eslint-disable-next-line
 const axios = require('axios');
@@ -17,11 +18,20 @@ function jwtSignPerson(person) {
 
 module.exports = {
   async signup_student(req, res) {
-      console.log(req);
+      console.log('student');
       try {
         const person = await Person.create(req.body);
 
         const personJson = person.toJSON();
+
+        const {dpName, acronym} = req.body;
+
+        const department = await Department.create({
+          name: dpName,
+          acronym: acronym,
+        });
+
+        console.log(department);
         res.send({
           person: personJson,
           token: jwtSignPerson(personJson),
@@ -34,6 +44,7 @@ module.exports = {
   },
   async signup_staff(req, res) {
     try {
+      console.log('staff');
       const person = await Person.create(req.body);
 
       const personJson = person.toJSON();
