74993e1bc5f204b64e13b55f36e4117a2a9b9a0c
Nuno Ramos <nunor07.unfc@gmail.com>
2018-04-19 19:56:02 +0100

    Testing netlify

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let baseURL;
 
-console.log('API ENV:' + process.env.API_ENV);
+console.log('API ENV test:' + process.env.API_ENV);
 
 if(process.env.NODE_ENV !== 'production'){
     baseURL = 'http://localhost:8081'
