3c0740e787723817a8059214a0e287f92e05a171
Paulo Santos <up201403745@fe.up.pt>
2018-04-25 00:00:36 +0100

    Trying to use enrollment and graduation date in coursestudent

--- server/src/models/Course.js
+++ server/src/models/Course.js
@@ -41,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
       // Course can have many students
       Course.belongsToMany(models.Student,
         {
-          through: 'CourseStudent',
+          through: models.CourseStudent,
           onDelete: 'CASCADE',
           onUpdate: 'CASCADE',
         }
