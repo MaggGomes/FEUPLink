499ea007258c6373fa5d1191762fae4702fdf44b
Jose Martins <lluismmartins7@gmail.com>
2018-04-25 18:23:43 +0100

    course CRUD with improved error and feedback support

--- client/src/components/pages/Management.vue
+++ client/src/components/pages/Management.vue
@@ -1,202 +1,160 @@
 <template>
   <v-container fluid>
-  <v-layout row wrap>
+    <v-layout row wrap>
+     
+         <!-- provide feedback to the user -->
+        <v-snackbar :timeout="6000" :color="feedbackColor" v-model="showingFeedback" top multi-line>
+          <div v-if="error!=null">
+            {{error.response.data.error}}
+          </div>
+          <div v-if="success!=null">
+            {{success.res}}
+          </div>
+          <v-btn flat dark @click="showingFeedback=false"> Close </v-btn>
+        </v-snackbar>
 
-    <!-- dialog new course --> 
-    <v-dialog v-model="courseDialog" persistent max-width="700px">
-      <v-card>
-        <v-card-title>
-          <span class="headline"> {{ updatingCourse ? 'Update Course information' : 'Create a new Course' }} </span>
-        </v-card-title>
-        <v-card-text>
-        <v-form v-model="formValid" ref="form" autocomplete="off" lazy-validation>
-          <v-container grid-list-md>
-            <v-layout wrap>
+        <!-- warning dialog -->
+        <v-dialog v-model="warningDialog">
+          <v-card>
+            <v-card-title class="headline">Be Careful!</v-card-title>
+            <v-card-text>
+              {{warningTitle}}
+            </v-card-text>
+            <v-card-actions>
+              <v-btn flat color="red" @click="warningDialog=false"> Cancel </v-btn>
+              <v-spacer> </v-spacer>
+              <v-btn flat color="green" @click="() => { warningDialog=false, warningAction() }"> Confirm </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
-              <v-flex xs12 md9>
-                <v-text-field
-                  label="Course name"
-                  hint="This must be unique"
-                  v-model="name"
-                  :rules="[v => !!v || 'Course name is required']"
-                  required
-                ></v-text-field>
-              </v-flex>
-              
-              <v-flex xs12 md3>
-                <v-text-field
-                  label="Course acronym"
-                  v-model="acronym"
-                  :rules="[v => !!v || 'Course acronym is required']"
-                  required
-                ></v-text-field>
-              </v-flex>
 
-              <v-flex xs12>
-                <v-text-field
-                  label="Description"
-                  v-model="description"
-                  multi-line
-                ></v-text-field>
-              </v-flex>
+        <!-- dialog new course -->
+        <v-dialog v-model="courseDialog" persistent max-width="700px">
+          <v-card>
+            <v-card-title>
+              <span class="headline"> {{ updatingCourse ? 'Update Course information' : 'Create a new Course' }} </span>
+            </v-card-title>
+            <v-card-text>
+              <v-form v-model="formValid" ref="form" autocomplete="off">
+                <v-container grid-list-md>
+                  <v-layout wrap>
 
-              <v-flex xs12 md9>
-                <v-text-field
-                  label="Website"                  
-                  v-model="website"     
-                  prepend-icon="web"             
-                ></v-text-field>
-              </v-flex>
+                    <v-flex xs12 md9>
+                      <v-text-field label="Course name" hint="This must be unique" v-model="name" :rules="[v => !!v || 'Course name is required']"
+                        required></v-text-field>
+                    </v-flex>
 
-              <v-flex xs12 md3>
-                <v-select
-                  label="Type of degree"                 
-                  v-model="academicDegree"
-                  :rules="[v => !!v || 'Course type of degree is required']"
-                  required
-                  :items="['Bachelor', 'Masters', 'PhD']"
-                ></v-select>
-              </v-flex>
+                    <v-flex xs12 md3>
+                      <v-text-field label="Course acronym" v-model="acronym" :rules="[v => !!v || 'Course acronym is required']" required></v-text-field>
+                    </v-flex>
 
-              <v-flex xs12 md6>
-               <v-menu
-                ref="creationDateMenu"
-                lazy
-                :close-on-content-click="false"
-                v-model="creationDateMenu"
-                transition="scale-transition"
-                offset-y
-                full-width
-                :nudge-right="40"
-                min-width="290px"
-                >
-                  <v-text-field
-                    slot="activator"
-                    label="Creation date"
-                    v-model="creationDate"
-                    :rules="[v => !!v || 'Course creation date is required']"
-                    required
-                    prepend-icon="event"
-                    readonly
-                  ></v-text-field>
-                  <v-date-picker
-                    ref="picker"
-                    v-model="creationDate"
-                    @change="saveCreationDate"
-                    min="1950-01-01"
-                    :max="new Date().toISOString().substr(0, 10)"
-                  ></v-date-picker>
-                </v-menu>
-              </v-flex>
+                    <v-flex xs12>
+                      <v-text-field label="Description" v-model="description" multi-line></v-text-field>
+                    </v-flex>
 
-                <v-flex xs12 md6>
-               <v-menu
-                ref="endDateMenu"
-                lazy
-                :close-on-content-click="false"
-                v-model="endDateMenu"
-                transition="scale-transition"
-                offset-y
-                full-width
-                :nudge-right="40"
-                min-width="290px"
-                >
-                  <v-text-field
-                    slot="activator"
-                    label="Close date"
-                    v-model="endDate"
-                    prepend-icon="event"
-                    hint="If the course has already closed"
-                    readonly
-                  ></v-text-field>
-                  <v-date-picker
-                    ref="picker"
-                    v-model="endDate"
-                    @change="saveEndDate"
-                    min="1950-01-01"
-                    :max="new Date().toISOString().substr(0, 10)"
-                  ></v-date-picker>
-                </v-menu>
-              </v-flex>
-              
-            </v-layout>
-          </v-container>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="closeDialog">Cancel</v-btn>
-          <v-btn 
-            color="blue darken-1"
-            flat
-            @click.native="updatingCourse? updateCourse() : createCourse()"
-            :disabled="!formValid"
-          >
-            {{ updatingCourse ? 'Update' : 'Create' }}
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+                    <v-flex xs12 md9>
+                      <v-text-field label="Website" v-model="website" prepend-icon="web"></v-text-field>
+                    </v-flex>
 
-    <v-jumbotron color="grey lighten-2">
-      <v-layout>
-        <v-flex xs6 md3>
-           <v-btn flat  color="success" slot="activator" @click="courseDialog=true">
-                <v-icon> add </v-icon>
-          </v-btn>
-        </v-flex>
-      </v-layout>
+                    <v-flex xs12 md3>
+                      <v-select label="Type of degree" v-model="academicDegree" :rules="[v => !!v || 'Course type of degree is required']" required
+                        :items="['Bachelor', 'Masters', 'PhD']"></v-select>
+                    </v-flex>
 
-      <!-- list of courses --> 
-      <v-layout mt-2 row wrap>
-        <v-flex xs12 offset-md1 md10 >
-          <v-expansion-panel v-for="course in courses" :key="course.id">
-            <v-expansion-panel-content>
-              <div slot="header">
-                <v-toolbar>
-                  <v-toolbar-title>{{course.name}}</v-toolbar-title>
-                  <v-spacer></v-spacer>
-                  <v-toolbar-items>
-                    <v-btn flat @click="openUpdateCourseDialog(course)">
-                      <v-icon> mode edit </v-icon>
-                    </v-btn>
-                    <v-btn flat color="error" @click="deleteCourse(course.id)">
-                      <v-icon> delete </v-icon>
-                    </v-btn>
-                  </v-toolbar-items>
-                </v-toolbar>
-              </div>
-              <v-card>
-                <v-card-text>{{course.description}}</v-card-text>
-              </v-card>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-      </v-flex>
+                    <v-flex xs12 md6>
+                      <v-menu ref="creationDateMenu" lazy :close-on-content-click="false" v-model="creationDateMenu" transition="scale-transition"
+                        offset-y full-width :nudge-right="40" min-width="290px">
+                        <v-text-field slot="activator" label="Creation date" v-model="creationDate" :rules="[v => !!v || 'Course creation date is required']"
+                          required prepend-icon="event" readonly></v-text-field>
+                        <v-date-picker ref="picker" v-model="creationDate" @change="saveCreationDate" min="1950-01-01" :max="new Date().toISOString().substr(0, 10)"></v-date-picker>
+                      </v-menu>
+                    </v-flex>
 
-      <!-- pagination -->
-      <v-flex xs12>
-        <div class="text-xs-center">
-          <v-pagination :length="6" v-model="page"></v-pagination>
-        </div>
-      </v-flex>
-</v-layout>
-</v-jumbotron>
+                    <v-flex xs12 md6>
+                      <v-layout>
+                      <v-flex xs10>
+                      <v-menu ref="endDateMenu" lazy :close-on-content-click="false" v-model="endDateMenu" transition="scale-transition" offset-y
+                        full-width :nudge-right="40" min-width="290px">
+                        <v-text-field slot="activator" label="Close date" v-model="endDate" prepend-icon="event" hint="If the course has already closed"
+                          readonly></v-text-field>
+                        <v-date-picker ref="picker" v-model="endDate" @change="saveEndDate" min="1950-01-01" :max="new Date().toISOString().substr(0, 10)"></v-date-picker>
+                      </v-menu>   
+                      </v-flex>
 
-<v-layout row wrap>
+                      <v-flex xs2>
+                      <v-btn flat color="blue-grey lighten-3" @click="endDate=''"> <v-icon> clear </v-icon> </v-btn>             
+                      </v-flex>
+                      </v-layout>
+                    </v-flex>
+                      
 
-</v-layout>
+                  </v-layout>
+                </v-container>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" flat @click.native="closeDialog">Cancel</v-btn>
+              <v-btn color="blue darken-1" flat @click.native="() => { 
+                  warningDialog=true
+                  if(updatingCourse){
+                    warningAction=updateCourse
+                    warningTitle='Are you sure you want to update the course?'
+                  }else{
+                    warningAction=createCourse
+                    warningTitle='Are you sure you want to create the course?'
+                  }
+                }" :disabled="!formValid">
+                {{ updatingCourse ? 'Update' : 'Create' }}
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
 
-<v-layout>
-  <v-flex xs12>
-    <v-alert type="warning" :value="true">
-      {{error}}
-    </v-alert>
-  </v-flex>
-</v-layout>
-</v-layout>
-</v-container>
+
+        
+          <v-flex xs6 md3>
+            <v-btn flat color="success" slot="activator" @click="courseDialog=true">
+              <v-icon> add </v-icon>
+            </v-btn>
+          </v-flex>
+        
+
+        <!-- list of courses -->
+       
+          <v-flex xs12>
+          
+                  <v-toolbar v-for="course in courses" :key="course.id">
+                    <v-toolbar-title>{{course.name}}</v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                      <v-btn flat @click="openUpdateCourseDialog(course)">
+                        <v-icon> mode edit </v-icon>
+                      </v-btn>
+                      <v-btn flat color="error" @click="() => {
+                          currentCourseId=course.id
+                          warningDialog=true
+                          warningAction=deleteCourse 
+                          warningTitle=`Are you sure you want to delete \'${course.name}\' ?`}">
+                        <v-icon> delete </v-icon>
+                      </v-btn>
+                    </v-toolbar-items>
+                  </v-toolbar>
+            
+          </v-flex>
+
+          <!-- pagination -->
+          <v-flex xs12>
+            <div class="text-xs-center">
+              <v-pagination :length="6" v-model="page"></v-pagination>
+            </div>
+          </v-flex> 
+
+
+    </v-layout>
+  </v-container>
 </template>
-
 
 
 <script>
@@ -206,9 +164,15 @@ import CourseService from '@/services/CourseService'
 export default {
   data () {
     return {    
-      showingError: false,
+      // warning dialog
+      warningDialog: false,
+      warningTitle: null,
+      // feedback
+      showingFeedback: false,
+      feedbackColor: 'error',
       error: null,
       success: null,
+      // pagination
       page: 1,
       courseDialog: false,
       // update
@@ -217,20 +181,22 @@ export default {
       // courses data
       courses: [],
       // course dialog fields
-      name: '',
-      academicDegree: '',
-      acronym: '',
-      description: '',
-      website: '',
-      creationDate: '',
+      name: null,
+      academicDegree: null,
+      acronym: null,
+      description: null,
+      website: null,
+      creationDate: null,
       creationDateMenu:false,
-      endDate: '',
+      endDate: null,
       endDateMenu:false,
       //form-validation
       formValid:false,
     }
   },
   methods: {
+    warningAction(){ // function to be ovewritten by the correct action
+    },
     saveCreationDate (date) {
 		  this.$refs.creationDateMenu.save(date)
 	  },
@@ -239,18 +205,8 @@ export default {
 	  },
     closeDialog(){
       this.courseDialog=false
-      this.updatingCourse=false
-      this.emptyDialogFields()
-    },
-    emptyDialogFields(){
-      this.name=''
-      this.academicDegree=''
-      this.acronym=''
-      this.description=''
-      this.website=''
-      this.creationDate=''
-      this.endDate=''
-    },
+      this.updatingCourse=false      
+    },    
     getCourseObject(){
       // returns the object with a course based on the dialog fields
       return {
@@ -268,14 +224,12 @@ export default {
       try{
         this.success = (await CourseService.create_course(this.getCourseObject())).data
 
-        // clean the dialog
-        this.courseDialog=false
-        this.emptyDialogFields()
+        this.closeDialog()
         
         // update course list
         this.getCourses()
       }catch(error){
-        this.error=error.response.data.error
+        this.error=error
       }
       
     },
@@ -286,8 +240,8 @@ export default {
       this.acronym=course.acronym
       this.description=course.description
       this.website=course.website
-      this.creationDate=String(course.creationDate).substring(0,10)
-      this.endDate=String(course.endDate).substring(0,10)
+      this.creationDate=(course.creationDate !== null) ? String(course.creationDate).substring(0,10) : course.creationDate
+      this.endDate=(course.endDate !== null) ? String(course.endDate).substring(0,10) : course.endDate
 
       this.updatingCourse=true
       this.courseDialog=true
@@ -309,21 +263,21 @@ export default {
       }
     
     },
-    async deleteCourse(id){
+    async deleteCourse(){
       try{
-        this.success = (await CourseService.delete_course(id)).data
+        this.success = (await CourseService.delete_course(this.currentCourseId)).data
         
         // update course list
         this.getCourses()
       }catch(error){
-        this.error=error;
+        this.error=error
       }
     },
     async getCourses(){
       try{
         this.courses = (await CourseService.list_all_courses()).data
       }catch(error){
-        this.error = error
+        this.error=error
       }
     }
   },
@@ -337,6 +291,25 @@ export default {
     },
     endDate (val) {
       val && this.$nextTick(() => (this.$refs.picker.activePicker = 'YEAR'))
+    },
+    courseDialog (val) {
+      // to clear red fields and form data
+      if(val && !this.updatingCourse)
+        this.$refs.form.reset()
+    },
+    success (val){
+      if(val !== null){
+        this.error=null
+        this.feedbackColor='success'
+        this.showingFeedback=true
+      }
+    },
+    error(val){
+      if(val !== null){
+        this.success=null
+        this.feedbackColor='error'
+        this.showingFeedback=true
+      }
     }
   },
 }
