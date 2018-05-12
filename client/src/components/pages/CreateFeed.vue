14ea5aa10a40d0ef707af61ce4067809c6e46389
Manuel Gomes <magg.slb@gmail.com>
2018-05-12 02:49:19 +0100

    fix

--- client/src/components/pages/CreateFeed.vue
+++ client/src/components/pages/CreateFeed.vue
@@ -155,7 +155,7 @@
         e1: 0,
         title: null,
         description: null,
-        text: '<p>Some initial content</p>',
+        text: '<p>Some content</p>',
         postType: null,
         postTypes: ['New', 'Job', 'Event', 'Education'],
         link: '',
