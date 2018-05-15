9c5928bf0a9c73a7c70a7e943e936feda2b1f161
Nuno Ramos <nunor07.unfc@gmail.com>
2018-05-15 16:27:34 +0100

    Fixing nav-bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,10 +5,8 @@
 
       <v-navigation-drawer app temporary v-model="sideNav" absolute>
         <v-list>
-          <v-list-tile v-if="$store.state.isUserLoggedIn">
-            <v-list-tile-content v-for="signnedInItem in signnedInMenuItens" :key="signnedInItem.title" route :to="signnedInItem.link">
-              {{ signnedInItem.title }}
-            </v-list-tile-content>
+          <v-list-tile v-if="$store.state.isUserLoggedIn" v-for="signnedInItem in signnedInMenuItens" :key="signnedInItem.title" route :to="signnedInItem.link">
+            {{ signnedInItem.title }}
           </v-list-tile>
           <v-list-tile v-else>
             <v-list-tile-content v-for="menuItem in menuItens" :key="menuItem.title" route :to="menuItem.link">
@@ -18,37 +16,50 @@
         </v-list>
       </v-navigation-drawer>
 
-      <v-toolbar floating dark id="main-bar">
-        <div id="side-main">
-          <v-toolbar-side-icon class="hidden-sm-and-up" @click.stop="sideNav = !sideNav">
-          </v-toolbar-side-icon>
-          <v-toolbar-title >
-            <router-link to="/" tag="span" style="cursor: pointer">
-              <v-avatar tile id="mini-logo" class="hidden-sm-and-up">
-                <img
-                  :src="minilogo"
-                  alt="IconFeup"
-                >
-              </v-avatar>
-              <v-avatar tile id="main-logo" class="hidden-xs-only">
-                <img
-                  v-bind:src="logo"
-                  alt="FEUPLink"
-                >
-              </v-avatar>              
-            </router-link>                    
-          </v-toolbar-title>
-        </div>
-        <div id="main-bar-content" class="hidden-xs-only">                 
-          <v-spacer></v-spacer>          
-          <v-toolbar-items flex v-if="$store.state.isUserLoggedIn" >
-            <v-btn flat v-for="signnedInItem in signnedInMenuItens" :class="signnedInItem.title" :key="signnedInItem.title" router :to="signnedInItem.link">{{ signnedInItem.title }}</v-btn>
-          </v-toolbar-items>
-          <v-toolbar-items flex v-else class="hidden-xs-only">
-            <v-btn flat v-for="menuItem in menuItens" :class="menuItem.title" :key="menuItem.title" router :to="menuItem.link">{{ $t(menuItem.title) }}</v-btn>
-          </v-toolbar-items>
+      <v-toolbar dark class="main-bar">
+        <v-container class="main-container">
+          <v-layout row>
 
-          <v-menu>
+            <v-flex class="hidden-sm-and-down">
+              <v-toolbar-title >
+                <router-link to="/" tag="span" style="cursor: pointer">
+                  <v-avatar tile class="main-logo">
+                    <img v-bind:src="logo" alt="FEUPLink">
+                  </v-avatar>              
+                </router-link>                    
+              </v-toolbar-title>
+            </v-flex>
+
+            <v-flex class="hidden-md-and-up">
+              <v-layout>
+                <v-toolbar-side-icon style="margin: 0px;" @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+                <v-flex>
+                  <v-layout justify-end>
+                  <v-toolbar-title>
+                    <router-link to="/" tag="span" style="cursor: pointer">
+                      <v-avatar tile class="mini-logo">
+                        <img :src="minilogo" alt="IconFeup">
+                      </v-avatar>             
+                    </router-link>                    
+                  </v-toolbar-title>
+                  </v-layout>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+                       
+          <v-flex class="main-bar-content hidden-sm-and-down">
+            <v-layout justify-end>    
+              <v-toolbar-items flex v-if="$store.state.isUserLoggedIn" >
+                <v-btn flat v-for="signnedInItem in signnedInMenuItens" :class="signnedInItem.title" :key="signnedInItem.title" router :to="signnedInItem.link">{{ signnedInItem.title }}</v-btn>
+                
+              </v-toolbar-items>
+              <v-toolbar-items flex v-else>
+                <v-btn flat v-for="menuItem in menuItens" :class="menuItem.title" :key="menuItem.title" router :to="menuItem.link">{{ $t(menuItem.title) }}</v-btn>
+              </v-toolbar-items>
+            </v-layout>
+          </v-flex>
+
+          <!-- <v-menu>
             <v-btn id="locale-dropdown" slot="activator">
               {{this.$i18n.locale}}
               <v-icon large left>arrow_drop_down</v-icon>
@@ -58,8 +69,9 @@
                 <v-list-tile-title>{{ language }}</v-list-tile-title>
               </v-list-tile>
             </v-list>
-          </v-menu>
-        </div>
+          </v-menu> -->
+          </v-layout>
+        </v-container>
       </v-toolbar>
 
       <v-content>
@@ -75,187 +87,148 @@
 </template>
 
 <script>
-  import logo from './assets/FEUPLinklogo.png';
-  import minilogo from './assets/IconFeup.png';
-  import Vue from 'vue';
+import logo from "./assets/FEUPLinklogo.png";
+import minilogo from "./assets/IconFeup.png";
+import Vue from "vue";
 
-  export default {
-    name: "App",
-    data() {
-      return {        
-        minilogo: minilogo,
-        logo: logo,
-        sideNav: false,
-        menuItens: [
-          {
-            title: 'signin',//"Sign-In",
-            link: "/signin"
-          },
-          {
-            title: 'signup',//"Sign-Up",
-            link: "/signup"
-          }
-        ]
-      };
-    },
-    methods: {
-      changeLanguage(language) {
-        this.$cookies.set('locale', language)
-        this.$i18n.locale = language
-      },
-    },
-    created: function(){
-      let locale = this.$cookies.get('locale')
-        if(locale === null){
-          locale = navigator.language //|| navigator.userLanguage
-          if(locale.includes('pt')) locale = 'pt'
-          else locale = 'en'
-          this.$cookies.set('locale',locale)
-        }        
-        this.$i18n.locale = locale
-    },
-    computed: {      
-      signnedInMenuItens() {
-        let userId = (this.$store.state.user != undefined ? this.$store.state.user.id : 0)
-        return [
-          {
-            title: "home",
-            link: "/"
-          },
-          {
-            title: "feed",
-            link: "/feed"
-          },
-          {
-            title: 'management',
-            link: "/management"
-          },
-          {
-            title: "profile",
-            link: "/profile/" + userId
-          },
-          {
-            title: "logout",
-            link: "/logout"
-          }
-        ];
-      }
-    },
+export default {
+  name: "App",
+  data() {
+    return {
+      minilogo: minilogo,
+      logo: logo,
+      sideNav: false,
+      menuItens: [
+        {
+          title: "signin", //"Sign-In",
+          link: "/signin"
+        },
+        {
+          title: "signup", //"Sign-Up",
+          link: "/signup"
+        }
+      ]
+    };
+  },
+  methods: {
+    changeLanguage(language) {
+      this.$cookies.set("locale", language);
+      this.$i18n.locale = language;
+    }
+  },
+  created: function() {
+    let locale = this.$cookies.get("locale");
+    if (locale === null) {
+      locale = navigator.language; //|| navigator.userLanguage
+      if (locale.includes("pt")) locale = "pt";
+      else locale = "en";
+      this.$cookies.set("locale", locale);
+    }
+    this.$i18n.locale = locale;
+  },
+  computed: {
+    signnedInMenuItens() {
+      let userId =
+        this.$store.state.user != undefined ? this.$store.state.user.id : 0;
+      return [
+        {
+          title: "Home",
+          link: "/"
+        },
+        {
+          title: "Feed",
+          link: "/feed"
+        },
+        {
+          title: "Management",
+          link: "/management"
+        },
+        {
+          title: "Profile",
+          link: "/profile/" + userId
+        },
+        {
+          title: "Logout",
+          link: "/logout"
+        }
+      ];
+    }
   }
+};
 </script>
 
 <style>
-  #locale-dropdown {
-    width:max-content;
-  }
 
-  #app {
-    font-family: "Avenir", Helvetica, Arial, sans-serif;
-    background-color: #EEEEEE;
-  }
-  #main-bar{
-    background-color: rgb(140,45,25);
-  }
-  .toolbar{
-    width:100%;
-    margin:0px;
-  }
-  .toolbar__content{
-    display: inline-flex;
-    width: 100%;
-  }
-  #main-bar .toolbar__content{
-    background: url('assets/header.png') no-repeat center;
-    background-size: cover;
-  }
-  #main-bar .btn__content{
-    text-transform: none;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  background-color: #eeeeee;
+}
+.main-bar {
+  background: url("assets/header.png") no-repeat center;
+  background-size: cover;
+  background-color: rgb(140, 45, 25);
+  margin-bottom: 2em;
+  height: 50px;
+}
+.main-bar .btn__content {
+  text-transform: none;
+}
 
-  #main-bar .btn__content:before{
-    opacity: 0;
-  }
+.main-bar .btn__content:before {
+  opacity: 0;
+}
 
-  #side-main{
-    width: 100%;
-    display: inline-flex;
-  }
+.main-logo {
+  height: 50px !important;
+  width: auto !important;
+}
+.mini-logo {
+  height: 40px !important;
+  width: auto !important;
+}
 
-  #main-bar-content{
-    width: 100%;
-    display: flex;
+@media (min-width: 991px) {
+  .main-bar {
+    height: 120px !important;
   }
+  .main-bar-content {
+    margin-top: 20px;
+  }
+  .main-bar-content .btn {
+    width: 120px;
+    border: 2px solid #f5f5f5;
+    border-radius: 2%;
+    color: rgb(140, 45, 25);
+    background-color: #f5f5f5 !important;
+    font-weight: bold;
+    margin-left: 2px;
+    margin-right: 2px;
+  }
+  .main-container {
+    margin-top: 30px !important;
+    margin-left: 30px !important;
+  }
+}
+.font-color {
+  color: white;
+}
 
-  #main-logo{
-    height: 50px!important;
-    width: auto!important;
+@media (max-width: 600px) {
+  .main-container {
+    margin-top: -1.3em !important;
   }
-  #mini-logo{
-    height: 40px!important;
-    width: auto!important;
-  }
+}
 
-  @media(min-width: 991px){
-    #main-bar .toolbar__content{
-      display: inline-block;
-      height: 145px!important;
-    }
-    #main-bar-content{
-      width: auto;
-      margin: 0px 50px;
-      color: rgb(140,45,25);
-    }
-    #side-main{
-      height: 50px;
-      margin-top: 50px;
-    }
-    #main-bar-content .btn{
-      width: 140px;
-      border: 2px solid white;
-      border-radius: 0%;
-      color:rgb(140,45,25);
-      background-color: white!important;
-    }
-    #main-bar-content .Sign-Up{
-      color: white;
-      background-color: transparent!important;
-    }
-
-    #main-bar-content .Sign-In,
-    #main-bar-content .Sign-Up{
-
-      margin-left: 20px!important;
-    }
-
+@media (min-width: 600px) and (max-width: 991px) {
+  .main-bar {
+    height: 70px;
   }
-  @media (max-width: 990px){
-    #main-logo{
-      height: 40px!important;
-    }
-  }
-  @media (max-width: 600px){
-    #side-main{
-      display: inline-flex;
-      margin-left: 0;
-    }
-    #side-main .btn{
-      margin-top: 0px;
-    }
-    #side-main .toolbar__title{
-      text-align: end;
-      flex:auto;
-      margin-right: 20px;
-    }
-  }
-  .font-color {
-    color: white;
-  }
+}
 
-  @media screen and (min-width: 960px) {
-    .max-width {
-      width: 80%;
-      margin: 0 auto;
-    }
+@media screen and (min-width: 960px) {
+  .max-width {
+    width: 80%;
+    margin: 0 auto;
   }
-
+}
 </style>
